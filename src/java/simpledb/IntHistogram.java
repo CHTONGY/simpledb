0dc9ddc285bae8dc42d190b6790a7034ecc1d130
Xiaoxuan <sunxiaox@usc.edu>
2021-11-28 11:41:17 -0500

    implement IntHistogram

--- src/java/simpledb/IntHistogram.java
+++ src/java/simpledb/IntHistogram.java
@@ -1,9 +1,15 @@
 package simpledb;
 
+import simpledb.Predicate.Op;
+
 /** A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
-
+    private int[] countBuckets;
+    private double width;
+    private int min;
+    private int max;
+    private int ntups;
     /**
      * Create a new IntHistogram.
      * 
@@ -22,6 +28,11 @@ public class IntHistogram {
      */
     public IntHistogram(int buckets, int min, int max) {
     	// some code goes here
+        this.countBuckets = new int[buckets];
+        this.min = min;
+        this.max = max;
+        this.width = Math.ceil((double)(max - min) / buckets);
+        ntups = 0;
     }
 
     /**
@@ -30,6 +41,13 @@ public class IntHistogram {
      */
     public void addValue(int v) {
     	// some code goes here
+        if(v == max) {
+            countBuckets[countBuckets.length - 1]++;
+        } else {
+            int bucketNo = (int)((v - min) / width);
+            countBuckets[bucketNo]++;
+        }
+        ntups++;
     }
 
     /**
@@ -43,9 +61,75 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
-
     	// some code goes here
+        // only consider <,<= , =, >=, >
+        int bucketNo = (int)((v - min) / width);
+        if(v == max) {
+            bucketNo = countBuckets.length - 1;
+        }
+        double left = min + bucketNo * width;
+        double right = left + width;
+        
+        if(op.equals(Predicate.Op.GREATER_THAN) || op.equals(Predicate.Op.GREATER_THAN_OR_EQ)) {
+            // corner case
+            if(v <= min) {
+                return 1;
+            }
+            if(v > max) {
+                return 0;
+            }
+            // (right-v)/width * h + rest of heights in the right
+            int restSum = 0;
+            for(int i = bucketNo + 1; i < countBuckets.length; i++) {
+                restSum += countBuckets[i];
+            }
+
+            double res = (((double)(right - v)) / width * countBuckets[bucketNo] + restSum) / ntups;
+            if(op.equals(Predicate.Op.GREATER_THAN_OR_EQ)) {
+                res += calEqual(v);
+            }
+            return res;
+            
+        } else if(op.equals(Predicate.Op.LESS_THAN) || op.equals(Predicate.Op.LESS_THAN_OR_EQ)) {
+            // corner case
+            if(v >= max) {
+                return 1;
+            }
+            if(v < min) {
+                return 0;
+            }
+            // (v-left)/width * h + rest of heights in the left
+            int restSum = 0;
+            for(int i = 0; i < bucketNo; i++) {
+                restSum += countBuckets[i];
+            }
+            double res = (((double)(v - left)) / width * countBuckets[bucketNo] + restSum) / ntups;
+            if(op.equals(Predicate.Op.LESS_THAN_OR_EQ)) {
+                res += calEqual(v);
+            }
+            return res;
+
+        } else if(op.equals(Predicate.Op.EQUALS)) {
+            // corner case
+            if(v > max || v < min) {
+                return 0;
+            }
+            // (h/w)/ntups
+            return ((double)countBuckets[bucketNo]) / width / ntups;
+        } else if(op.equals(Predicate.Op.NOT_EQUALS)) {
+            // corner case
+            if(v > max || v < min) {
+                return 1;
+            }
+
+            return 1 - ((double)countBuckets[bucketNo]) / width / ntups;
+        }
         return -1.0;
+    }
+
+    private double calEqual(int v) {
+        int bucketNo = (int)((v - min) / width);
+        return ((double)countBuckets[bucketNo]) / width / ntups; 
     }
     
     /**
